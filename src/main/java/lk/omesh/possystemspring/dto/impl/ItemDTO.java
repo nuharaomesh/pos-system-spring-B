275ee9fe6891f51c6fc9a4ac13350bf0f5f28905
nuharaomesh <omeshnuhara4@gmail.com>
2024-10-04 13:34:21 +0530

    impl ItemsStatus

--- src/main/java/lk/omesh/possystemspring/dto/impl/ItemDTO.java
+++ src/main/java/lk/omesh/possystemspring/dto/impl/ItemDTO.java
@@ -1,5 +1,6 @@
 package lk.omesh.possystemspring.dto.impl;
 
+import lk.omesh.possystemspring.dto.ItemStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class ItemDTO {
+public class ItemDTO implements ItemStatus {
     private String itemID;
     private String itemName;
     private String category;
