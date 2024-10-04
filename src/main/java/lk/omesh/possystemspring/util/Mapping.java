097831e45732d2f05e39963f940f945479d1feed
nuharaomesh <omeshnuhara4@gmail.com>
2024-10-04 13:34:46 +0530

    impl item mappings

--- src/main/java/lk/omesh/possystemspring/util/Mapping.java
+++ src/main/java/lk/omesh/possystemspring/util/Mapping.java
@@ -1,8 +1,9 @@
 package lk.omesh.possystemspring.util;
 
-import lk.omesh.possystemspring.dto.CustomerStatus;
 import lk.omesh.possystemspring.dto.impl.CustomerDTO;
+import lk.omesh.possystemspring.dto.impl.ItemDTO;
 import lk.omesh.possystemspring.entity.impl.Customer;
+import lk.omesh.possystemspring.entity.impl.Item;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,17 @@ public class Mapping {
 
     public List<CustomerDTO> getCustomerDTOList(List<Customer> customerList) {
         return modelMapper.map(customerList, new TypeToken<List<CustomerDTO>>(){}.getType());
+    }
+
+    public Item toItemEntity(ItemDTO dto) {
+        return modelMapper.map(dto, Item.class);
+    }
+
+    public ItemDTO toItemDTO(Item entity) {
+        return modelMapper.map(entity, ItemDTO.class);
+    }
+
+    public List<ItemDTO> getItemDTOList(List<Item> itemList) {
+        return modelMapper.map(itemList, new TypeToken<List<ItemDTO>>(){}.getType());
     }
 }
