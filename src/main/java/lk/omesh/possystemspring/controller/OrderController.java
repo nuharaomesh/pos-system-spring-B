012d6faccbeb9f591dc06ae7e5bb675e96f9aed7
nuharaomesh <omeshnuhara4@gmail.com>
2024-10-07 20:48:27 +0530

    add order controller logs

--- src/main/java/lk/omesh/possystemspring/controller/OrderController.java
+++ src/main/java/lk/omesh/possystemspring/controller/OrderController.java
@@ -2,6 +2,8 @@ package lk.omesh.possystemspring.controller;
 
 import lk.omesh.possystemspring.exception.DataPersistException;
 import lk.omesh.possystemspring.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,26 +13,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("api/v1/orders")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
+    static Logger log = LoggerFactory.getLogger(OrderController.class);
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveOrder(@RequestBody String jsonList) {
 
         try {
-            orderService.saveItem(jsonList);
+            orderService.placeOrder(jsonList);
+            log.info("Order Successfully Saved!");
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (DataPersistException e) {
-            e.printStackTrace();
+            log.info("Order Not Saved" + e.getMessage());
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.info("Internal Server error" + e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
