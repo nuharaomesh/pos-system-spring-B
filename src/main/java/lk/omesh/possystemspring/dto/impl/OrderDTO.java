781339256d859ebc00b671613bd02c87788033a7
nuharaomesh <omeshnuhara4@gmail.com>
2024-10-07 20:25:05 +0530

    update orderDTO

--- src/main/java/lk/omesh/possystemspring/dto/impl/OrderDTO.java
+++ src/main/java/lk/omesh/possystemspring/dto/impl/OrderDTO.java
@@ -4,12 +4,42 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class OrderDTO {
-    private String orderID;
-    private float price;
-    private String time;
-    private int qty;
+    private List<Item> items;
+    private Order order;
+    private List<OrderItem> orderItems;
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    public static class Item {
+        private String itemID;
+        private int qty;
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    public static class Order {
+        private String orderID;
+        private float price;
+        private String time;
+        private int qty;
+        private String customerID;
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    public static class OrderItem {
+        private String orderID;
+        private String itemID;
+        private double price;
+        private int counts;
+    }
 }
